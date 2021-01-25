02cf8801af09a6ca531330316d950258ff6347ed
Darrall Flowers <dpflowersjr@alaska.edu>
2021-01-25 14:00:22 -0900

    fixed test and implemented parsing given dictonary to calculate number of specified days

--- src/url_parser.py
+++ src/url_parser.py
@@ -13,13 +13,34 @@ def get_latest_publish_date(url):
 
     news_Feed = feedparser.parse(url)
     entry = news_Feed.entries[0]
+    if 'GMT' in entry.get('published'):
+        timezone_adjustment = str(entry.get('published')).replace('GMT', "+0000")
+        return datetime.strptime(timezone_adjustment,"%a, %d %b %Y %H:%M:%S %z")
+
     latest_publish_date = datetime.strptime(str(entry.get('published')), "%a, %d %b %Y %H:%M:%S %z")
 
     return latest_publish_date
 
 
 def get_num_days_without_activity(date):
+    # subtract the number of dates via milisecods to get result of days away from current
     current_date = datetime.now().replace(tzinfo=utc)
     count = current_date - date.replace(tzinfo=utc)
     return count.days
 
+def find_company_with_inactivity_for_given_num_of_days( dict , num_inactive_days):
+
+    #loop through dict and check days of inactivity
+    companies = []
+
+    try:
+        for company in dict:
+            latest_date = get_latest_publish_date(dict.get(company))
+            inactive_day_count = get_num_days_without_activity(latest_date)
+            if inactive_day_count == num_inactive_days:
+                companies.append(company)
+
+        return companies
+    except:
+        traceback.print_exc()
+    return companies
--- url_parser_test.py
+++ url_parser_test.py
@@ -1,25 +1,50 @@
 import unittest
+import datetime
+
+import pytest
 
 from src.url_parser import get_latest_publish_date
 from src.url_parser import get_num_days_without_activity
+from src.url_parser import find_company_with_inactivity_for_given_num_of_days
 
-#Given a Dictionary keyed by Company and valued by RSS feed url, write a function that determines which companies had no activity for a given number of days.
+
+
 class MyTestCase(unittest.TestCase):
-    def setUp(self):
-        url_1 = "http://joeroganexp.joerogan.libsynpro.com/rss"
-        pass
 
     def test_get_latest_publish_date(self):
-        url_1 = "http://joeroganexp.joerogan.libsynpro.com/rss"
-        print(get_latest_publish_date(url_1))
-        self.assertTrue(True)
+        test_url = "http://joeroganexp.joerogan.libsynpro.com/rss"
+
+        # check that reponse is a date time obj
+        print(get_latest_publish_date(test_url))
+
+        self.assertTrue(isinstance(get_latest_publish_date(test_url), datetime.date))
 
     def test_get_num_days_without_activity(self):
-        url_1 = "http://joeroganexp.joerogan.libsynpro.com/rss"
-        latest_pub_date = get_latest_publish_date(url_1)
+        test_url = "http://joeroganexp.joerogan.libsynpro.com/rss"
+        latest_pub_date = get_latest_publish_date(test_url)
         print(get_num_days_without_activity(latest_pub_date))
         self.assertTrue(True)
 
+    #On Mon Jan 25 2021 the result will be the Joe Rogan Expirence
+    def test_find_company_with_inactivity_for_given_num_of_days(self):
+        companies_rss_dict = dict({
+            'The Joe Rogan Experience': "http://joeroganexp.joerogan.libsynpro.com/rss",
+            "1619 by The New York Times": "https://feeds.simplecast.com/6HzeyO6b",
+            "Code Switch by NPR": "https://feeds.npr.org/510312/podcast.xml",
+            "Crime Junkie by audiochuck": "https://feeds.megaphone.fm/ADL9840290619",
+            "Unlocking Us with Brené Brown": "https://feeds.megaphone.fm/unlocking-us",
+            "The Daily by The New York Times": "https://feeds.simplecast.com/54nAGcIl",
+            "The Ben Shapiro Show by The Daily Wire": "https://feeds.megaphone.fm/WWO8086402096",
+            "My Favorite Murder by Karen Kilgariff and Georgia Hardstark": "https://www.omnycontent.com/d/playlist/aaea4e69-af51-495e-afc9-a9760146922b/44bbf446-4627-4f83-a7fd-ab070007db11/72b96aa8-88bd-480a-87af-ab070007db36/podcast.rss",
+            "Call Her Daddy by Barstool Sports": "https://mcsorleys.barstoolsports.com/feed/call-her-daddy",
+            "Dateline NBC": "https://podcastfeeds.nbcnews.com/dateline-nbc",
+        })
+
+        result = find_company_with_inactivity_for_given_num_of_days(companies_rss_dict, 58)
+        print(result)
+        self.assertEquals(result ,['The Joe Rogan Experience'])
+
+
 if __name__ == '__main__':
     unittest.main()
-# feeds need to be taken in as a list , then parsed one by one
+
